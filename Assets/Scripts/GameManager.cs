3ba996b09453af957806832ecb6a67855a077ca6
William <quagsire089@gmail.com>
2025-09-04 15:20:00 -0600

    Added Indicato and managers

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(instance);
+        }
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void EndGame()
+    {
+
+    }
+}
--- Assets/Scripts/MomControl.cs
+++ Assets/Scripts/MomControl.cs
@@ -4,10 +4,12 @@ using UnityEngine.AI;
 public class MomControl : MonoBehaviour
 {
     private NavMeshAgent _momAgent;
+    [SerializeField] private float _speed;
 
     private void Start()
     {
         _momAgent = GetComponent<NavMeshAgent>();
+        _momAgent.speed = _speed;
     }
 
 
